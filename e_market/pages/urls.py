f7b8d6371ceecd2b4bc8988856a4bf6a85fd7f27
ChrisBreidy <53558330+ChrisBreidy@users.noreply.github.com>
2021-03-16 23:02:05 +0200

    Fixed the listings page, added the register html file and login html file. Also, i linked the reg html file and login html file to the viewspy and urls.py in the pages folder.

--- e_market/pages/urls.py
+++ e_market/pages/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),#root path,name is a name that will allow us to reference this path through it
     path('about',views.about,name='about'),
+    path('register',views.register,name='register'),
+    path('login', views.login, name='login'),
+
 ]
